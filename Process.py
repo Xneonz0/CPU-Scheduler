35d060a82da76f2b2662e88ac15fd89e62a12b92
Xneonz0 <yousefalhajbaddar@gmail.com>
2025-01-12 18:10:17 +0300

    Process.py

--- Process.py
+++ Process.py
@@ -1,12 +1,14 @@
 class Process:
-    def __init__(self, pid, arrival, burst, queue):
+    def __init__(self, pid, arrival, burst, queue, priority=3):
         self.pid = pid
         self.arrival = arrival
         self.burst = burst
         self.queue = queue
+        self.priority = priority  
         self.remaining_time = burst
         self.start_time = None
         self.finish_time = None
 
     def __str__(self):
-        return f"PID: {self.pid}, Arrival: {self.arrival}, Burst: {self.burst}, Queue: {self.queue}"
+        return (f"PID: {self.pid}, Arrival: {self.arrival}, Burst: {self.burst}, "
+                f"Queue: {self.queue}, Priority: {self.priority}")
